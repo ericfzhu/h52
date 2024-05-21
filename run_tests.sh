580d549b4f972d413369ce6576a8754fe3445c51
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-05-22 00:00:48 +1000

    script no longer works

--- run_tests.sh
+++ run_tests.sh
@@ -6,17 +6,11 @@ LAMBDA_FUNCTION_DIR="functions/hermes_profiler"
 # Build the Docker image for the Lambda function
 docker build -t lambda-function-test -f "${LAMBDA_FUNCTION_DIR}/Dockerfile" "${LAMBDA_FUNCTION_DIR}"
 
-# Create a virtual environment for tests
-python -m venv test_venv
-
-# Activate the test virtual environment
-source test_venv/bin/activate
-
-# Install the test dependencies
-pip install -r tests/requirements.txt
-
 # Run the unittests inside the Docker container
-docker run --rm -v "$(pwd)/tests:/var/task/tests" lambda-function-test python -m unittest discover tests/
-
-# Deactivate the test virtual environment
-deactivate
+docker run --rm \
+  -v "$(pwd)/tests:/var/task/tests" \
+  -v "$(pwd)/functions:/var/task/functions" \
+  -v "$(pwd)/tests/requirements.txt:/var/task/requirements.txt" \
+  -e AWS_DEFAULT_REGION='us-west-2' \
+  --entrypoint "" \
+  lambda-function-test /bin/bash -c "pip install -r /var/task/requirements.txt && python -m unittest discover tests/"
