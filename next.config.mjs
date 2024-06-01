fc8b023a227a9779150dcd37b3265f54f1cec309
Eric Zhu <73148494+ericfzhu@users.noreply.github.com>
2024-06-02 00:28:19 +1000

    prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "export",
+	reactStrictMode: true,
+	swcMinify: true,
+	webpack(config) {
+		config.experiments = {
+		asyncWebAssembly: true,
+		layers: true,
+		};
+
+		return config;
+	},
+	images: {
+		unoptimized: true,
+	},
+	experimental: {
+		scrollRestoration: true,
+	},
+};
 
 export default nextConfig;
